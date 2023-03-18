3654205ae730737cdd6ac1465d79e9c809a31df6
ManInTheMirrow <75084095+ManInTheMirrow@users.noreply.github.com>
2023-03-18 23:27:48 +0300

    Update and rename omni_BQru-5745L.mk to twrp_BQru-5745L.mk

--- twrp_BQru-5745L.mk
+++ twrp_BQru-5745L.mk
@@ -7,16 +7,10 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
-# Inherit from BQru-5745L device
-$(call inherit-product, device/bqru/BQru-5745L/device.mk)
+# Inherit from TWRP common configurations.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
 
@@ -28,7 +22,7 @@ PRODUCT_PACKAGES += \
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := BQru-5745L
-PRODUCT_NAME := omni_BQru-5745L
+PRODUCT_NAME := twrp_BQru-5745L
 PRODUCT_BRAND := BQru
 PRODUCT_MODEL := BQ-5745L
 PRODUCT_MANUFACTURER := BQru
