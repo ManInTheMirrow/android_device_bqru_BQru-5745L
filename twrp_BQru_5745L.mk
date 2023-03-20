fc161c00ca7f622a02f81f7a2b7ba357fcab1bb0
ManInTheMirrow <75084095+ManInTheMirrow@users.noreply.github.com>
2023-03-20 03:16:50 +0300

    Update twrp_BQru_5745L.mk

--- twrp_BQru_5745L.mk
+++ twrp_BQru_5745L.mk
@@ -18,8 +18,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # Inherit from TWRP common configurations.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
-    $(LOCAL_PATH)/prebuilt/dtb.img:dtb.img
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
 
 # Extra required packages
 PRODUCT_PACKAGES += \
