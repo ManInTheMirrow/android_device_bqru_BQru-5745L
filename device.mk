f21f7c35b63de22d5d8df96633faeee2daa52f8d
ManInTheMirrow <75084095+ManInTheMirrow@users.noreply.github.com>
2023-08-22 03:58:33 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -7,6 +7,8 @@
 
 LOCAL_PATH := device/bqru/BQru_5745L
 
+PRODUCT_PLATFORM := sp9863a
+
 # Dynamic Partitions stuff
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
@@ -16,21 +18,39 @@ PRODUCT_TARGET_VNDK_VERSION := 30
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
-# A/B
+# Virtual A/B
 ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += ro.apex.updatable=false
 
 # A/B
+AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
+    uboot \
+    sml \
+    trustos \
+    teecfg \
     vbmeta \
     vbmeta_system \
     vbmeta_vendor \
+    vbmeta_product \
+    vbmeta_system_ext \
     dtbo \
+    l_ldsp \
+    l_gdsp \
+    l_modem \
+    l_deltanv \
+    wcnmodem \
+    gpsgl \
+    gpsbd \
+    pm_sys \
     boot \
     system \
     system_ext \
     vendor \
-    product
+    product \
+    socko \
+    odmko
 
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
@@ -42,24 +62,23 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Health Hal
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl.recovery \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-service \
+    android.hardware.boot@1.1-impl.recovery
 
 PRODUCT_PACKAGES += \
-    bootctrl.unisoc \
-    bootctrl.unisoc.recovery
+    bootctrl.$(PRODUCT_PLATFORM) \
+    bootctrl.$(PRODUCT_PLATFORM).recovery
 
 # Fastbootd
 PRODUCT_PACKAGES += \
-    fastbootd \
     android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
 
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
